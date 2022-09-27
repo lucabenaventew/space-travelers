97171f4d41e76273a2ce3c06a91228eeaefe4e76
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-27 15:23:45 -0500

    Add cases fail, loading & succed - missions

--- src/Redux/Missions/missions.js
+++ src/Redux/Missions/missions.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const URL = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
+  status: 'idle',
   missions: [],
 };
 
@@ -15,6 +17,21 @@ export const fetchMissions = createAsyncThunk('missions/fetchMissions', async ()
 export const missionsSlice = createSlice({
   name: 'missions',
   initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.missions = action.payload;
+    });
+    builder.addCase(fetchMissions.pending, (state) => {
+        state.status = 'loading';
+        }
+    );
+    builder.addCase(fetchMissions.rejected, (state) => {
+        state.status = 'failed';
+        }
+    );
+  },
 });
 
 //   export const { setBook, unsetBook } = booksSlice.actions;
