14a25fd055d464cbaf6284e14e674f3e7461ad7c
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-27 15:13:30 -0500

    Fetch API

--- src/Redux/Missions/missions.js
+++ src/Redux/Missions/missions.js
@@ -1,8 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const URL = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
     missions: [],
 };
+
+export const fetchMissions = createAsyncThunk('missions/fetchMissions', async () => {
+    const response = await fetch(URL);
+    const missionsData = await response.json();
+    return missionsData;
+});
 
 export const missionsSlice = createSlice({
     name: 'missions',
