ddb5dbfa4d17cb84ed555a3ff80c4b985ae414d8
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-27 15:15:15 -0500

    Fix eslint

--- src/Redux/Missions/missions.js
+++ src/Redux/Missions/missions.js
@@ -3,21 +3,20 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const URL = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
-    missions: [],
+  missions: [],
 };
 
 export const fetchMissions = createAsyncThunk('missions/fetchMissions', async () => {
-    const response = await fetch(URL);
-    const missionsData = await response.json();
-    return missionsData;
+  const response = await fetch(URL);
+  const missionsData = await response.json();
+  return missionsData;
 });
 
 export const missionsSlice = createSlice({
-    name: 'missions',
-    initialState,
+  name: 'missions',
+  initialState,
 });
-  
+
 //   export const { setBook, unsetBook } = booksSlice.actions;
-  
-  export default missionsSlice.reducer;
-  
+
+export default missionsSlice.reducer;
