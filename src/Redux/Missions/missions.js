b7d6fa54f993ec4ce2d6d7d827c99c6d4513fb6c
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-27 11:08:07 -0500

    Fix error in Redux.

--- src/Redux/Missions/missions.js
+++ src/Redux/Missions/missions.js
@@ -1,7 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
+import rockets from '../Rockets/rockets';
+
+const initialState = {
+    missions: [],
+};
 
 export const missionsSlice = createSlice({
     name: 'missions',
+    initialState,
 });
   
 //   export const { setBook, unsetBook } = booksSlice.actions;
--- src/Redux/Rockets/rockets.js
+++ src/Redux/Rockets/rockets.js
@@ -1,7 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+    rockets: [],
+};
+
 export const rocketsSlice = createSlice({
     name: 'rockets',
+    initialState,
 });
 
 export default rocketsSlice.reducer;
