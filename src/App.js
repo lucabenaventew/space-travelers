d794b591a337b12014246aaabe04dcd5c24be19e
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-27 16:30:41 -0500

    Fix Router Missions no working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <h2 className="navbar-logo">logo</h2>
         <ul className="navbar-elements">
           <li className="navbar-element"><Link className="navtext" to="/">Rockets</Link></li>
-          <li className="navbar-element"><Link className="navtext" to="/">Missions</Link></li>
+          <li className="navbar-element"><Link className="navtext" to="/Missions">Missions</Link></li>
           <li className="navbar-element"><Link className="navtext" to="/">My Profile</Link></li>
         </ul>
       </nav>
@@ -18,9 +18,9 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<Missions />}
+          element="{}"
         />
-        <Route path="/categories" element={<Missions />} />
+        <Route path="/Missions" element={<Missions />} />
         {/* <Route path="/categories" element={<Profile />} /> */}
       </Routes>
     </>
