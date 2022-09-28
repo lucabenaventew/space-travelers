95049d0a0e6a80fc29df99409daa3e57f445dd7e
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-28 12:53:04 -0500

    Add fixed width to join row

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -43,7 +43,7 @@ function Missions() {
           <th className="head-table">Mission</th>
           <th className="head-table">Description</th>
           <th className="head-table">Status</th>
-          <th className="head-table"> Join </th>
+          <th className="row-4 head-table"> Join </th>
         </tr>
         {domTemplate()}
       </table>
