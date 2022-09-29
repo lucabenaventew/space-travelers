4f6f44e3b5148ff8b4774f7c4e6ea974b7af7f7d
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-29 13:58:48 -0500

    Add ClassName to MyProfile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './MyProfile.css';
 
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rockets);
+  console.log(rockets);
+  const missions = useSelector((state) => state.missions.missions);
+  const activeMissions = missions.filter((mission) => mission.reserved);
+
   const rocketsReservation = rockets.filter((rocket) => rocket.reserved);
+  console.log(rocketsReservation);
+
   return (
     <>
       <div className="my-profile-section">
         <div className="rockets-profile">
-          <h2>Rockets</h2>
-          <ul>
+          <h2 className="profile-title">Rockets</h2>
+          <ul className="profile-list">
             {rocketsReservation.length === 0
               ? <li className="no-rocket-reservations">No Reservations</li>
               : rocketsReservation.map((rocket) => (
@@ -23,9 +30,13 @@ const MyProfile = () => {
           </ul>
         </div>
         <div className="missions-profile">
-          <h2>Missions</h2>
-          <ul>
-            <p>Missions content</p>
+          <h2 className="profile-title">Missions</h2>
+          <ul className="profile-list">
+            {activeMissions.map((mission) => (
+              <li className="mission-reserved" key={mission.mission_id}>
+                {mission.mission_name}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
